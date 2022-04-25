730cf8b57a21e2c750992bd9ad92ffc161b91ea8
dooblpls <dooblpls@users.noreply.github.com>
2022-04-25 16:07:21 +0200

    init

---
+++ Start-CitrixKeepAlive.ps1
@@ -0,0 +1,22 @@
+﻿while(1){
+[System.Reflection.Assembly]::LoadFile("C:\Program Files (x86)\Citrix\ICA Client\WfIcaLib.dll")
+$ICA = New-Object WFICALib.ICAClientClass
+
+$handle= $ICA.EnumerateCCMSessions()
+$sessionCount = $ICA.GetEnumNameCount($handle)
+
+for($i=0;$i -lt $sessionCount;$i++){
+$session = $ICA.GetEnumNameByIndex($handle, 0)
+$ICA.SetProp("OutputMode", "1")
+$ICA.StartMonitoringCCMSession($session, $true)
+$ICA.Session.Keyboard.SendKeyDown(66)
+$ICA.StopMonitoringCCMSession($session)
+
+}
+
+
+$ICA.CloseEnumHandle($handle)
+
+sleep 30
+
+}
